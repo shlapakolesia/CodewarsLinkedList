4ef462e91f5ccd6c53d694ccbb3aee3c94fafe7a
llesssia <shlapak.olesia@gmail.com>
2026-03-27 22:37:03 +0200

    Create solution.py
    Implemented loop_size function to detect a cycle in a linked list

---
+++ can_you_get_the_loop/solution.py
@@ -0,0 +1,18 @@
+def loop_size(node):
+    slow = node
+    fast = node
+
+    while True:
+        slow = slow.next
+        fast = fast.next.next
+        if slow == fast:
+            break
+
+    count = 1
+    current = slow.next
+
+    while current != slow:
+        count += 1
+        current = current.next
+
+    return count
